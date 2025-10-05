ad859dcf3afe37c5ec91c0c7f3ddd032a4af6ab1
Tamanna Sanjana <tamannasanjana57@gmail.com>
2025-10-06 00:26:53 +0600

    Tamanna

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple JS for showing alert when contact form is submitted
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thank you for reaching out! (Form is not functional yet)");
+});
